0fe8d583ab2d4dfd3e69d9d2aa0e553d96186f34
Anna Mazurek <anna.mazurek.93@gmail.com>
2019-12-27 08:30:31 +0100

    Add border

--- script.js
+++ script.js
@@ -3,13 +3,16 @@ To do:
 - change text
 - change font
 - css person--back (center, line-height, itd.)
+- rwd!
 */
 const card = document.querySelector('.card');
 const button = document.querySelector('.button-flip');
 const buttonBack = document.querySelector('.button-flip--back');
+const cardBack = document.querySelector('.person--back');
 
 
 button.addEventListener("click", () => {
+  cardBack.style.display = 'flex';
   card.style.transform = 'rotateY(180deg)';
 });
 buttonBack.addEventListener("click", () => {
