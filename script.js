6a6085bcec9f88ba899810fc5bb380d111a75e0c
Anna Mazurek <anna.mazurek.93@gmail.com>
2020-01-07 09:34:18 +0100

    Back to RWD / small style changes

--- script.js
+++ script.js
@@ -9,13 +9,19 @@ const card = document.querySelector('.card');
 const button = document.querySelector('.button-flip');
 const buttonBack = document.querySelector('.button-flip--back');
 const cardBack = document.querySelector('.person--back');
+const cardFront = document.querySelector('.person--front');
 
 
 button.addEventListener("click", () => {
   cardBack.style.display = 'flex';
+  cardFront.style.display = 'none';
   card.style.transform = 'rotateY(180deg)';
 });
 buttonBack.addEventListener("click", () => {
   card.style.transform = 'rotateY(0deg)';
+  cardFront.style.display = 'flex';
+  cardBack.style.display = 'none';
+
+
 });
 
